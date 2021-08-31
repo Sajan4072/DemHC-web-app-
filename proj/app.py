c0aecf1b23a06e08f3a794f200c4435195b673ac
Sajan4072 <rai4072@gmail.com>
2021-08-31 20:25:44 +0545

    login and registration classes

--- proj/app.py
+++ proj/app.py
@@ -1,3 +1,4 @@
+from enum import unique
 import os
 import sys
 
@@ -5,6 +6,14 @@ import sys
 from flask import Flask, request, render_template, Response, jsonify, url_for
 from flask_sqlalchemy import SQLAlchemy 
 from flask_login import UserMixin
+from flask_wtf import wtforms
+from flask_wtf.form import FlaskForm
+from flask_wtf.recaptcha import validators
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import InputRequired,Length,ValidationError
+
+
+
 #from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
@@ -53,9 +62,31 @@ def model_predict(img, model):
 
 class User(db.Model,UserMixin):
     id=db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(20),nullable=False)
+    username=db.Column(db.String(20),nullable=False,unique=True)
     password=db.Column(db.String(80),nullable=False)
 
+class RegisterForm(FlaskForm):
+    username=StringField(validators=[InputRequired(),Length(min=4,max=20)],render_kw={"placeholder":"Username"})
+
+    password=PasswordField(validators=[InputRequired(),Length(min=4,max=20)],render_kw={"placeholder":"Password"})
+
+    submit=SubmitField("signup")
+
+    def validate_username(self,username):
+        existing_user_username=User.query.filter_by(
+            username=username.data).first()
+
+        if existing_user_username:
+            raise ValidationError(
+                "This username is taken please choose another"
+            )    
+
+class LoginForm(FlaskForm):
+    username=StringField(validators=[InputRequired(),Length(min=4,max=20)],render_kw={"placeholder":"Username"})
+
+    password=PasswordField(validators=[InputRequired(),Length(min=4,max=20)],render_kw={"placeholder":"Password"})
+
+    submit=SubmitField("login")
 
 
 

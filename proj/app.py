42c4d088b3da9b3e9f3138c63ec3294bc6e455af
Sajan4072 <rai4072@gmail.com>
2021-09-03 18:38:25 +0545

    first skeleton of working articles added

--- proj/app.py
+++ proj/app.py
@@ -11,7 +11,7 @@ from flask_wtf.recaptcha import validators
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import InputRequired,Length,ValidationError
 from flask_bcrypt import Bcrypt
-
+from datetime import datetime
 
 
 #from werkzeug.utils import secure_filename
@@ -70,6 +70,13 @@ def model_predict(img, model):
     
     return preds
 
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
 
 class User(db.Model,UserMixin):
     id=db.Column(db.Integer,primary_key=True)
@@ -103,12 +110,38 @@ class LoginForm(FlaskForm):
 
 
 @app.route('/article')
+@login_required
 def article():
-    return render_template('article.html')
+    posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
+    return render_template('article.html',posts=posts)
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
+@app.route('/post/<int:post_id>')
+@login_required
+def post(post_id):
+    post = Blogpost.query.filter_by(id=post_id).one()
+    return render_template('post.html',post=post)
+
+
+@app.route('/add')
+@login_required
+def add():
+    return render_template('add.html')
+
+
+@app.route('/addpost', methods=['POST'])
+@login_required
+def addpost():
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    author = request.form['author']
+    content = request.form['content']
+
+    post = Blogpost(title=title, subtitle=subtitle, author=author, content=content, date_posted=datetime.now())
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(url_for('article'))    
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -155,6 +188,7 @@ def logout():
 
 
 @app.route('/predict', methods=['GET', 'POST'])
+@login_required
 def predict():
     if request.method == 'POST':
         
